3e0eebeb3450018688b0b251e5e2b52fe6d89026
Dai Gonçs <daianeegoncs@gmail.com>
2026-06-01 21:44:02 -0300

    add front end

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import NovaDenuncia from './pages/NovaDenuncia';
+
+const routes = {
+  login: Login,
+  register: Register,
+  dashboard: Dashboard,
+  denuncia: NovaDenuncia,
+};
+
+export default routes;
